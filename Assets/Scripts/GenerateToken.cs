bcc313f30e29e1b768afeb1ae2bda0170d635f8f
Dipak Sisodiya <dipaksisodiya@Dipaks-MacBook-Air.local>
2022-04-27 01:20:36 +0530

    Token Generation added

--- Assets/Scripts/GenerateToken.cs
+++ Assets/Scripts/GenerateToken.cs
@@ -10,8 +10,11 @@ public class GenerateToken
     public static Token GetToken(string RoomName)
     {
         HttpWebRequest request;
-        string data = "{\"sessionId\":\"" + RoomName + "\"," +
-            "\"apiKey\":\"27fd6f8080d512442a3694f461adb3986cda5ba39dbe368d75\",\"user\":{\"name\":\"UnityUser\",\"moderator\": false}}";
+        string data = "{\"apiKey\":\"27fd6f8080d512442a3694f461adb3986cda5ba39dbe368d75\"," +
+				"\"user\":{\"id\":\"99266\"," +
+				"\"name\":\"Dipak\"," +
+				"\"email\":\"dipak.work.14@gmail.com\"," +
+				"\"avatar\":\"null\"}}";
 		byte[] dataBytes = Encoding.UTF8.GetBytes(data);
 		Debug.Log("Creating request");
 		request = (HttpWebRequest)(WebRequest.Create("https://api.sariska.io/api/v1/misc/generate-token"));
--- Assets/Scripts/SariskaChatUnity.cs
+++ Assets/Scripts/SariskaChatUnity.cs
@@ -2,9 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Phoenix;
+using WebSocketSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+
+public sealed class BasicLogger : Phoenix.ILogger
+{
+    public void Log(Phoenix.LogLevel level, string source, string message)
+    {
+        Console.WriteLine("[{0}]: {1} - {2}", level, source, message);
+    }
+}
 
 public class SariskaChatUnity : MonoBehaviour
 {
+
+    WebSocketHelper webSocketHelper;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +33,43 @@ public class SariskaChatUnity : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void GeneralIntegrationTest()
+    {
+        // SetUp
+        var onOpenCount = 0;
+
+        void OnOpenCallback()
+        {
+            onOpenCount++;
+        }
+
+        List<string> onErrorData = new();
+
+        void OnErrorCallback(string message)
+        {
+            onErrorData.Add(message);
+        }
+
+        // connecting is synchronous as implemented above
+        var socketAddress = $"wss://api.sariska.io/api/v1/messaging/websocket/websocket";
+        var socketFactory = new WebSocketHelper.WebsocketSharpFactory();
+        var socket = new Socket(
+            socketAddress,
+            null,
+            socketFactory,
+            new Socket.Options(new JsonMessageSerializer())
+            {
+                ReconnectAfter = _ => TimeSpan.FromMilliseconds(200),
+
+            }
+            );
+
+        socket.OnOpen += OnOpenCallback;
+        socket.OnError += OnErrorCallback;
+
+        socket.Connect();
+
     }
 }
